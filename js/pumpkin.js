bc3f418debfdf4e39c0f3d76e2b7940a405aa606
ju3tin <ju3tin@hotmail.co.uk>
2024-10-21 12:57:10 +0100

    wer

---
+++ js/pumpkin.js
@@ -0,0 +1,137 @@
+class Pumpkin{
+    constructor(x,y,rad){
+        this.x=x;
+        this.y=y;
+        this.rad=rad;
+    }
+    draw(ctx, openness){
+        ctx.save();
+        ctx.translate(this.x,this.y);
+        ctx.scale(this.rad,this.rad);
+
+    /*    ctx.fillStyle="gray";
+        ctx.beginPath();
+        ctx.rect(-1,-1,2,2);
+        ctx.fill();
+*/
+        this.#drawHead(ctx);
+        this.#drawEyes(ctx,openness);
+        this.#drawNose(ctx,openness);
+        this.#drawMouth(ctx,openness);
+       
+       
+        ctx.restore();
+    }
+
+    #drawMouth(ctx,openness){
+        ctx.fillStyle="black";
+        ctx.save();
+        ctx.translate(0,0.4);
+        ctx.scale(1-openness*0.2,0.2+openness);
+
+        ctx.moveTo(-0.6,0);
+        ctx.lineTo(-0.4,-0.17);
+        ctx.lineTo(-0.2,-0.08);
+        ctx.lineTo(0,-0.2);
+        ctx.lineTo(+0.2,-0.08);
+        ctx.lineTo(+0.4,-0.17);
+        ctx.lineTo(0.6,0);
+        ctx.lineTo(+0.4,0.17);
+        ctx.lineTo(+0.2,0.08);
+        ctx.lineTo(0,0.2);
+        ctx.lineTo(-0.2,0.08);
+        ctx.lineTo(-0.4,0.17);
+        ctx.closePath();
+        ctx.fill();
+
+        ctx.restore();
+    }
+
+    #drawNose(ctx,openness){
+        ctx.save();
+        ctx.translate(-0.08,-openness*0.05);
+
+        ctx.fillStyle="black";
+        ctx.beginPath();
+        ctx.moveTo(-0.05,0);
+        ctx.lineTo(0.05,-0.05);
+        ctx.lineTo(0.05,0.05);
+        ctx.closePath();
+        ctx.fill();
+
+        ctx.restore();
+
+        ctx.save();
+        ctx.translate(0.08,-openness*0.05);
+
+        ctx.fillStyle="black";
+        ctx.beginPath();
+        ctx.moveTo(0.05,0);
+        ctx.lineTo(-0.05,-0.05);
+        ctx.lineTo(-0.05,0.05);
+        ctx.closePath();
+        ctx.fill();
+
+        ctx.restore();
+
+
+    }
+    #drawEyes(ctx,openness){
+        ctx.save();
+        ctx.translate(-0.4,-0.4);
+        ctx.scale(1+openness*0.3,1.2-openness);
+
+        ctx.fillStyle="black";
+        ctx.beginPath();
+        ctx.moveTo(-0.15,0);
+        ctx.lineTo(0.15,-0.15);
+        ctx.lineTo(0.15,0.15);
+        ctx.closePath();
+        ctx.fill();
+
+        ctx.restore();
+
+        ctx.save();
+        ctx.translate(0.4,-0.4);
+        ctx.scale(1+openness*0.3,1.2-openness);
+
+        ctx.fillStyle="black";
+        ctx.beginPath();
+        ctx.moveTo(0.15,0);
+        ctx.lineTo(-0.15,-0.15);
+        ctx.lineTo(-0.15,0.15);
+        ctx.closePath();
+        ctx.fill();
+
+        ctx.restore();
+
+
+    }
+    #drawHead(ctx){
+        ctx.fillStyle="green";
+        ctx.beginPath();
+        ctx.rect(-0.05,-1,0.1,0.1);
+        ctx.fill();
+
+        ctx.fillStyle="rgb(255,150,0)";
+        ctx.beginPath();
+        ctx.ellipse(-0.6,0.03,0.4,0.92,0,0,Math.PI*2);
+        ctx.fill();
+        ctx.beginPath();
+        ctx.ellipse(0.6,0.03,0.4,0.92,0,0,Math.PI*2);
+        ctx.fill();
+
+        ctx.fillStyle="rgb(255,170,0)";
+        ctx.beginPath();
+        ctx.ellipse(-0.3,0.03,0.4,0.95,0,0,Math.PI*2);
+        ctx.fill();
+        ctx.beginPath();
+        ctx.ellipse(0.3,0.03,0.4,0.95,0,0,Math.PI*2);
+        ctx.fill();
+
+        ctx.fillStyle="rgb(255,190,0)";
+        ctx.beginPath();
+        ctx.ellipse(0,0.03,0.4,0.97,0,0,Math.PI*2);
+        ctx.fill();
+    }
+}
